e40d8f86c279f71ac71f341ba5847b3457e6e566
Casey Ledbetter <caseyledbetter@Caseys-MacBook-Pro.local>
2017-06-16 13:51:18 -0700

    Added formulas for the mortgage calc, and replaced all instances of var with 'vm.')

--- index.js
+++ index.js
@@ -7,7 +7,64 @@ angular
 function HomeController() {
     var vm = this;
 
+(function($) {
+	$.fn.calculateMortgage = function(options) {
+		vm.defaults = {
+			currency: '$',
+			params: {}
+		};
+		options = $.extend(defaults, options);
+		
+		vm.calculate = function(params) {
+			params = $.extend({
+				balance: 0,
+				rate: 0,
+				term: 0,
+				period: 0
+			}, params);
+			
+			vm.N = params.term * params.period;
+			vm.I = (params.rate / 100) / params.period;
+			vm.v = Math.pow((1 + I), N);
+			vm.t = (I * v) / (v - 1);
+			vm.result = params.balance * t;
+			
+			return result;
+		
+		};
+		
+		return this.each(function() {
+			vm.$element = $(this);
+			vm.$result = calculate(options.params);
+			vm.output = '<h4>' + options.currency + ' ' + $result.toFixed(2) + '</h4>';
+			$(output).appendTo($element);
+		
+		
+		});
+	
+	};
 
+
+})(jQuery);
+
+$(function() {
+	$('#test').on('submit', function(e) {
+		e.preventDefault();
+		vm.$params = {
+			balance: $('#balance').val(),
+			rate: $('#rate').val(),
+			term: $('#term').val(),
+			period: $('option:selected', '#period').val()
+		};
+		
+		$(this).calculateMortgage({
+			params: $params
+		})
+	
+	});	
+
+
+});
 
 
 
